844d4dc6f24a15811c75dd82241e4e50413d6705
Freek Post <freakpost@gmail.com>
2016-08-11 22:54:55 +0200

    Update.

--- src/Pokapi/Utility/Hex.php
+++ src/Pokapi/Utility/Hex.php
@@ -1,14 +1,28 @@
 <?php
 namespace Pokapi\Utility;
 
+/**
+ * Class Hex
+ *
+ * @package Pokapi\Utility
+ * @author Freek Post <freek@kobalt.blue>
+ */
 class Hex
 {
 
+    /**
+     * @param float $float
+     * @return string
+     */
     public static function float2hex(float $float) : string
     {
         return dechex(unpack('Q', pack('d', $float))[1]);
     }
 
+    /**
+     * @param float $float
+     * @return string
+     */
     public static function d2h(float $float)
     {
         return pack("H*", self::float2hex($float));
--- src/Pokapi/Utility/Signature.php
+++ src/Pokapi/Utility/Signature.php
@@ -1,25 +1,68 @@
 <?php
 namespace Pokapi\Utility;
 
+/**
+ * Class Signature
+ *
+ * @package Pokapi\Utility
+ * @author Freek Post <freek@kobalt.blue>
+ */
 class Signature
 {
+
+    /**
+     * Generate location1 hash
+     *
+     * @param string $authTicket
+     * @param float $latitude
+     * @param float $longitude
+     * @param float $altitude
+     *
+     * @return number
+     */
     public static function generateLocation1(string $authTicket, float $latitude, float $longitude, float $altitude = 0.0)
     {
         $seed = hexdec(xxhash32($authTicket, 0x1B845238));
         return hexdec(xxhash32(self::getLocationBytes($latitude, $longitude, $altitude), $seed));
     }
 
+    /**
+     * Generate location2 hash
+     *
+     * @param float $latitude
+     * @param float $longitude
+     * @param float $altitude
+     *
+     * @return int
+     */
     public static function generateLocation2(float $latitude, float $longitude, float $altitude = 0.0) : int
     {
-        return (int)hexdec(xxhash32(self::getLocationBytes($latitude, $longitude, $altitude), (int)hexdec(0x1B845238)));
+        return hexdec(xxhash32(self::getLocationBytes($latitude, $longitude, $altitude), 0x1B845238));
     }
 
+    /**
+     * Generate request hash
+     *
+     * @param string $authTicket
+     * @param string $request
+     *
+     * @return int
+     */
     public static function generateRequestHash(string $authTicket, string $request) : int
     {
-        $seed = (int)hexdec(xxhash64($authTicket, (int)hexdec(0x1B845238)));
-        return (int)hexdec(xxhash64($request, $seed));
+        $seed = hexdec(xxhash64($authTicket, 0x1B845238));
+        return hexdec(xxhash64($request, $seed));
     }
 
+    /**
+     * Get the location as bytes
+     *
+     * @param float $latitude
+     * @param float $longitude
+     * @param float $altitude
+     *
+     * @return string
+     */
     protected static function getLocationBytes(float $latitude, float $longitude, float $altitude) : string
     {
         return Hex::d2h($latitude) . Hex::d2h($longitude) . Hex::d2h($altitude);
